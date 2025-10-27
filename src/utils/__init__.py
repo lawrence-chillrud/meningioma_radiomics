2176a2c160765e7e4816b1474cf6c2cf10a62a4c
lawrence-chillrud <chili@u.northwestern.edu>
2025-10-27 15:50:15 -0500

    Adding get feats method

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,4 +1,5 @@
 from .get_cohort import get_cohort
+from .get_feats import get_feats
 from .get_mris import get_mris
 from .get_segs import get_segs, get_segs_roi_key
 from .io import read_ndarray, write_ndarray
---
+++ src/utils/get_feats.py
@@ -0,0 +1,76 @@
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+
+from .paths import LABELS_FILE
+
+
+def clean_feature_names(strings):
+    """Tidies up the feature names by removing hard-coded substrings."""
+    replacements = [
+        "Mod-AX_3D_",
+        "Mod-SAG_3D_",
+        "_POST",
+        "Mod-AX_",
+        "SegLab-",
+        "Feat-original_",
+    ]
+
+    # Replace specified substrings with ""
+    replaced_strings = []
+    for string in strings:
+        for r in replacements:
+            string = string.replace(r, "")
+        # Replace "DIFFUSION" with "DWI"
+        string = string.replace("DIFFUSION", "DWI")
+        replaced_strings.append(string)
+
+    return replaced_strings
+
+
+def get_feats(
+    prediction_task, features_path, labels_path=LABELS_FILE, scaler="Standard"
+):
+    # Read in features, labels, & merge
+    features = pd.read_csv(features_path)
+    labels = pd.read_csv(labels_path)
+    labels = labels.dropna(subset=[prediction_task])
+    labels = labels[labels["Subject Number"].isin(features["Subject Number"])]
+    data = features.merge(labels, on="Subject Number")
+
+    # Clean up column names
+    data.columns = clean_feature_names(data.columns)
+
+    # Drop metadata columns to isolate dataset
+    data = data.dropna(axis=1, how="all").fillna(0)
+    y = data[prediction_task].values.astype(int)
+    X = data.drop(
+        columns=[
+            "Subject Number",
+            "MethylationSubgroup",
+            "Chr1p",
+            "Chr22q",
+            "Chr9p",
+            "TERT",
+        ]
+    )
+    X = X.drop(columns=[col for col in X.columns if "-7-" in col])  # NAWM feats
+
+    # Drop cols w/constant values
+    constant_feats = [col for col in X.columns if X[col].nunique() == 1]
+    X = X.drop(columns=constant_feats)
+
+    # Scale feats
+    if scaler == "Standard":
+        scaler_obj = StandardScaler()
+    elif scaler == "MinMax":
+        scaler_obj = MinMaxScaler()
+    else:
+        scaler_obj = None
+
+    if scaler_obj is not None:
+        X = pd.DataFrame(scaler_obj.fit_transform(X), columns=X.columns)
+
+    # Subject IDs
+    sub_nos = data["Subject Number"].values
+
+    return X, y, sub_nos
