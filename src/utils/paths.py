d57b99874c0ba4cd807969bde17b91971e125678
lawrence-chillrud <chili@u.northwestern.edu>
2025-12-09 12:07:19 -0600

    Updating paths file to use latest feats file

--- src/utils/paths.py
+++ src/utils/paths.py
@@ -22,7 +22,7 @@ SEGS_DIR = PREPROC_DIR / "8_ALL_SMOOTH_SEGS_02-08-25"
 LABELS_FILE = PREPROC_DIR / "0_LABELS" / "MeningiomaBiomarkerData.csv"
 PULSES = ["T1_POST", "FLAIR", "DIFFUSION", "ADC"]
 ROIS = [1, 3, 4, 5, 6, 13, 15, 16, 156, 22]
-PYRAD_FILE = PYRAD_DIR / "10-10-2025_15-40-01" / "features_wide.csv"
+PYRAD_FILE = PYRAD_DIR / "12-08-2025_19-00-57_features.csv"
 
 # --- ensure directories exist ---
 for p in [
